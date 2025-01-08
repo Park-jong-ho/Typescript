807d893ecff3e22c826c488e7f532eab14296f0a
jongho <whjk1104@gmail.com>
2025-01-08 14:17:22 +0900

    Objects type

---
+++ 01.08.Type/index.ts
@@ -0,0 +1,22 @@
+// 배열 타입 방법 1 : type[]
+
+  // let nums: number[] = [1,2,3,4,5]
+  // let strs: string[] = ["a","b","c"]
+  // let bools: boolean[] = [false, false, true]
+
+  // nums.push(6)
+  // nums.push(7)
+
+  // bools.push(1)
+
+
+
+// 배열 타입 방법 2 : Array<>
+
+  // let nums: Array<number> = [1,2,3,4,5]
+  // let strs: Array<string> = ["a","b","c"]
+
+  // nums.push(6)
+  // nums.push(7)
+
+//
